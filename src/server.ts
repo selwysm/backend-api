1fec128d2c6c7281eef7a3fdeca0e25990534c7c
selwysm <mendozaselwys@gmail.com>
2024-11-13 22:17:10 -0500

    fix string

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 4000;
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Auth-service running on port ${PORT}`);
+  console.log(`Backend-service running on port ${PORT}`);
 });
